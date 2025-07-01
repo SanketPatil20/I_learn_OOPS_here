0a64fb798b74a4349fa9421a220ec3f14ab2835e
SanketPatil20 <sanketvp01@gmail.com>
2025-07-02 00:45:23 +0530

    Method overloading

--- src/Polymorphism_5/MethodOverloading.java
+++ src/Polymorphism_5/MethodOverloading.java
@@ -1,13 +1,25 @@
 package Polymorphism_5;
 
 class Test{
-    void show(){
+
+    // method overloading with same method show();
+
+    void show(){ // 0 arguments
         System.out.println("Calling without arguments");
     }
 
-    void show(int a){
+    void show(int a){ // 1 argument
         System.out.println("Calling with int argument");
     }
+
+    void show(String b){ // 1 argument
+        System.out.println("Calling with string argument");
+    }
+
+    void show(int c,String d){ // 1 argument
+        System.out.println("Calling with first argument int and second string");
+    }
+
 }
 
 
@@ -15,6 +27,10 @@ public class MethodOverloading {
 
     public static void main(String[] args) {
         Test t = new Test();
+
         t.show(1);
+        t.show("Hello there !!!");
+        t.show(10,"Apples");
+
     }
 }
