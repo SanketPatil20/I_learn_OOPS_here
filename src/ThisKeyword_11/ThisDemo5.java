4c1eebfcfe0d4c779c62e6ac1c51f2aca7cdb3f5
SanketPatil20 <sanketvp01@gmail.com>
2025-07-09 09:31:11 +0530

    This keyword to return current class instance

---
+++ src/ThisKeyword_11/ThisDemo5.java
@@ -0,0 +1,14 @@
+package ThisKeyword_11;
+
+public class ThisDemo5 {
+
+    ThisDemo5 m1(){
+        System.out.println("returning current class instance");
+        return this;
+    }
+
+    public static void main(String[] args) {
+        ThisDemo5 t = new ThisDemo5();
+        t.m1();
+    }
+}
