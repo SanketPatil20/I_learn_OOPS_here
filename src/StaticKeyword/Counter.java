ca5e36ebdf220b8a642ddabcd5d048aba986d4eb
SanketPatil20 <sanketvp01@gmail.com>
2025-07-15 23:44:01 +0530

    Counter using static keyword

---
+++ src/StaticKeyword/Counter.java
@@ -0,0 +1,18 @@
+package StaticKeyword;
+
+public class Counter {
+
+    static int count = 0;  //Counter will increase with every new object created
+
+    Counter(){
+        count++;
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+
+    }
+}
