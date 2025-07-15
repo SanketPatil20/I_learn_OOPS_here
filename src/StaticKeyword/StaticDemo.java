0fcd13b14e333fbfa5ef0fbcffdaf5bcb1348b9f
SanketPatil20 <sanketvp01@gmail.com>
2025-07-15 23:33:30 +0530

    Static keyword in java

--- src/StaticKeyword/StaticDemo.java
+++ src/StaticKeyword/StaticDemo.java
@@ -1,11 +1,28 @@
 package StaticKeyword;
 
-public class StaticDemo {
+class Employee {
+    int empid;
+    String name;
+    static String company = "Cisco";
 
-    static int a = 10;
-
-    void m1(){
-        static int b = 20;
+    void display() {
+        System.out.println(empid + " " + name + " " + company);
     }
 
+    Employee(int empid, String name){
+        this.empid = empid;
+        this.name = name ;
+    }
+}
+
+public class StaticDemo {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee(101, "Sanket");
+        e1.display();
+        Employee e2 = new Employee(102, "Tejas");
+        e2.display();
+
+    }
 }
